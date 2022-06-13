ef4ac261c5d55b52fb1dfdfc591f964f5aa2ea09
Bogdan Laban <auroraborealismcc@gmail.com>
2022-06-13 14:52:43 +0200

    s3 lambda trigger

---
+++ s3-lambda-trigger.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
+  bucket = "ldi-${var.env}-multi-desc-search-batch"
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "Input/"
+    filter_suffix       = ".csv"
+  }
+}
+resource "aws_lambda_permission" "permission" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::ldi-${var.env}-multi-desc-search-batch"
+}
