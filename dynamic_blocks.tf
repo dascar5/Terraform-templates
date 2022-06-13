09f1246756358510ae7a78199dd1a67bc5d390a3
Bogdan Laban <auroraborealismcc@gmail.com>
2022-06-13 18:13:25 +0200

    dynamic blocks

---
+++ dynamic_blocks.tf
@@ -0,0 +1,30 @@
+locals {
+  ips = {
+    "10.0.1.0/24" = "AWS1 VPC RDP Hosts"
+    "10.0.9.0/24" = "AWS1 VPC RDP Hosts"
+    "10.129.109.0/24" = "AWS VPN"
+    "10.9.184.0/24" = "aws workspaces"
+    "172.23.192.0/23" = "DC3 Jump Servers"
+    "172.23.206.0/23" = "VPN Subnet"
+    "172.23.208.0/21" = "VPN Subnet"
+    "172.23.240.0/21" = "VPN Subnet"
+    "172.29.192.0/23" = "TR2 Jump Servers"
+    "172.29.206.0/23" = "VPN Subnet"
+    "172.29.208.0/21" = "VPN Subnet"
+    "172.29.240.0/21" = "VPN Subnet"
+    "172.26.192.0/20" = "TWM"
+    "192.168.102.0/24" = "Intens VPN"
+  }
+}
+
+  dynamic "ingress" {
+    for_each = local.ips
+    content {
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+      cidr_blocks = [ingress.key]
+      description = ingress.value
+      security_groups = ["${data.aws_ssm_parameter.ui_sg_alb_id.value}","${data.aws_ssm_parameter.ui_sg_container_id.value}","${data.aws_ssm_parameter.searchwatch_sg.value}","${data.aws_ssm_parameter.process_sg.value}"]
+    }
+  }
