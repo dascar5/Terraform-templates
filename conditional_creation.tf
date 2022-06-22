bf2401f9ac212c104636bc899e998a2992dbd156
Bogdan Laban <auroraborealismcc@gmail.com>
2022-06-22 16:41:30 +0200

    Hacky conditional

--- conditional_creation.tf
+++ conditional_creation.tf
@@ -10,9 +10,41 @@ resource "aws_api_gateway_rest_api" "apig" {
   }
 }
 
-
 variable "api_gateway" {
   type        = bool
   default     = false
   description = ""
+}
+
+#-------------------------------------------------------------------------
+
+#hacky conditional
+
+locals{
+  create = length(regexall("dev", var.env)) > 0 ? true : false
+}
+
+variable "create"{
+  type = bool 
+  default = true
+  description = ""
+}
+#dev domain
+resource "aws_api_gateway_domain_name" "dev-domain" {
+  count = var.create ? 1 : 0
+  domain_name = "ldi-apig.dev.mlnonprod.liiaws.net"
+  certificate_arn = "arn:aws:acm:us-east-1:336990213410:certificate/3da0109f-6da2-4f6a-a5c8-fccef7c3e09b"
+}
+
+resource "aws_route53_record" "dev-record" {
+  count = var.create ? 1 : 0
+  name    = aws_api_gateway_domain_name.dev-domain[count.index].domain_name
+  type    = "A"
+  zone_id = "Z03602322VWTWFYJDB6N0"
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.dev-domain[count.index].cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.dev-domain[count.index].cloudfront_zone_id
+  }
 }
